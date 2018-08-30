d56deb4f3d21a4356d2e9ae50f294f56b001c169
chenliang <chasegalxy@163.com>
2018-08-30 21:35:15 +0800

    support new zhihu

--- src/main/java/zhiHuUserSprider/service/main/Main.java
+++ src/main/java/zhiHuUserSprider/service/main/Main.java
@@ -7,14 +7,16 @@ import zhiHuUserSprider.service.util.JsoupUtils;
 
 
 public class Main {
-	public static void main(String[] args) {
-		String url = "https://www.zhihu.com/people/yao-cheng-46/";// 初始解析网页地址
+	public static void main(String[] args) throws InterruptedException {
+		String url = "https://www.zhihu.com/people/li-si-si-59/";// 初始解析网页地址
 
 		while(true){
 			// 设置代理ip
 			HttpUtils.setProxyIp();
 			Document document = JsoupUtils.getDocument(url);// 得到的document一定是正常 的document
 			System.out.println(document);
+
+			//Thread.sleep(60000);
 		}
 	}
 }
--- src/main/java/zhiHuUserSprider/service/spriderService.java
+++ src/main/java/zhiHuUserSprider/service/spriderService.java
@@ -36,13 +36,14 @@ public class spriderService {
     static PreparedStatement ps = null;
 
     static Executor executor = Executors.newFixedThreadPool(20);
+
     public static void main(String[] args) throws InterruptedException {
         //从我的主页开始爬
 //        urlQueue.put("https://www.zhihu.com/people/yao-cheng-46");
-        urlQueue.put("https://www.zhihu.com/people/yanwenya");
-        System.out.println("初始队列大小:"+urlQueue.size());
+        urlQueue.put("https://www.zhihu.com/people/chasegalaxy");
+        System.out.println("初始队列大小:" + urlQueue.size());
         System.out.println("开始爬虫.........................................");
-        for (int i=0;i<5;i++){
+        for (int i = 0; i < 5; i++) {
             Thread a = new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -54,7 +55,7 @@ public class spriderService {
                             if (tmp != null) {
                                 crawler(tmp);
                             }
-                        }else {
+                        } else {
                             System.out.println("此url存在，不爬了." + tmp);
                         }
                     }
@@ -65,10 +66,10 @@ public class spriderService {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true){
+                while (true) {
                     try {
-                    //    System.out.println("当前活动线程数："+((ThreadPoolExecutor)executor).getActiveCount());
-                        if(((ThreadPoolExecutor)executor).getActiveCount()<5){
+                        //    System.out.println("当前活动线程数："+((ThreadPoolExecutor)executor).getActiveCount());
+                        if (((ThreadPoolExecutor) executor).getActiveCount() < 5) {
                             Thread a = new Thread(new Runnable() {
                                 @Override
                                 public void run() {
@@ -80,14 +81,14 @@ public class spriderService {
                                             if (tmp != null) {
                                                 crawler(tmp);
                                             }
-                                        }else {
+                                        } else {
                                             System.out.println("此url存在，不爬了." + tmp);
                                         }
                                     }
                                 }
                             });
                             executor.execute(a);
-                            if (urlQueue.size()==0){
+                            if (urlQueue.size() == 0) {
                                 System.out.println("队列为0了！！！！！！！！！！1");
                             }
                         }
@@ -103,7 +104,7 @@ public class spriderService {
     public static String getAUrl() {
         String tmpAUrl;
         try {
-            tmpAUrl= urlQueue.take();
+            tmpAUrl = urlQueue.take();
             return tmpAUrl;
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -111,148 +112,140 @@ public class spriderService {
         return null;
     }
 
+    private static void setUserInfo1(Element userUrlContent, ZhiHuUserBean user) {
+        Elements elements = userUrlContent.select(".ProfileHeader-contentBody");
+        System.out.println("spriderService.setUserInfo1");
+    }
     public static void crawler(String tmp) {
+        //解析网页
+        Element userUrlContent = Jsoup.parse(getFromUrl(tmp));
+        String userContent = userUrlContent.text();
+        ZhiHuUserBean user = new ZhiHuUserBean();
 
-                Pattern pattern;
-                Matcher matcher;
-                //Jsoup解析网页
-                Element userUrlContent = null;
+        //姓名
+        String name = userUrlContent.select(".ProfileHeader-name").first().text();
+        user.setName(name);
+        //行业 公司 职位
+        int size = userUrlContent.select(".ProfileHeader-infoItem").size();
 
-                //解析网页
-                userUrlContent = Jsoup.parse(getFromUrl(tmp));
-                String userContent = userUrlContent.text();
-                ZhiHuUserBean user = new ZhiHuUserBean();
+        //userUrlContent.select(".App-main").select("div[itemprop=people]");
 
-//                System.out.println(userContent.substring(userContent.lastIndexOf("获得")+3,userContent.indexOf("次感谢")-1));
+        //setUserInfo1(userUrlContent, user);
+        if (size == 2) {
+            String string1 = userUrlContent.select(".ProfileHeader-infoItem").first().text();
+            if (string1 != null && string1 != "") {
+                String[] a = string1.split(" ");
+                //行业
+                if (a.length > 0) {
+                    user.setBusiness(a[0]);
+                }
+                //公司
+                if (a.length > 1) {
+                    user.setCompany(a[1]);
+                }
+                //职位
+                if (a.length > 2) {
+                    user.setPosition(a[2]);
+                }
+            }
+            String string2 = userUrlContent.select(".ProfileHeader-infoItem").get(1).text();
+            if (string2 != null && string2 != "") {
+                String[] a = string2.split(" ");
+                //学校
+                if (a.length > 0) {
+                    user.setEducation(a[0]);
+                }
+                //专业
+                if (a.length > 1) {
+                    user.setMajor(a[1]);
+                }
+            }
+        }
 
-                    //姓名
-                    String name = userUrlContent.select(".ProfileHeader-name").first().text();
-                    user.setName(name);
-                    //行业 公司 职位
-                    int size = userUrlContent.select(".ProfileHeader-infoItem").size();
-                    if (size == 2) {
-                        String string1 = userUrlContent.select(".ProfileHeader-infoItem").first().text();
-                        if (string1 != null && string1 != "") {
-                            String[] a = string1.split(" ");
-                            //行业
-                            for (int i = 0; i < a.length; i++) {
-                                if (a.length > 0) {
-                                    user.setBusiness(a[0]);
-                                }
-                                //公司
-                                if (a.length > 1) {
-                                    user.setCompany(a[1]);
-                                }
-                                //职位
-                                if (a.length > 2) {
-                                    user.setPosition(a[2]);
-                                }
-                            }
-                        }
-                        String string2 = userUrlContent.select(".ProfileHeader-infoItem").get(1).text();
-                        if (string2 != null && string2 != "") {
-                            String[] a = string2.split(" ");
-                            //学校
-                            if (a.length > 0) {
-                                user.setEducation(a[0]);
-                            }
-                            //专业
-                            if (a.length > 1) {
-                                user.setMajor(a[1]);
-                            }
-                        }
-                    }
+        //看‘关注他’中有无关键字，判断性别
+        //String sexString = userUrlContent.select(".ProfileHeader-contentFooter button[icon]").first().text();
 
-                    //看‘关注他’中有无关键字，判断性别
-                    String sexString = userUrlContent.select(".ProfileHeader-contentFooter button[icon]").first().text();
-                    if (sexString.contains("他")) {
-                        user.setSex(0);
-                    } else if (sexString.contains("她")) {
-                        user.setSex(1);
-                    } else {
-                        user.setSex(2);
-                    }
+        Elements metaData = userUrlContent.select(".App-main").select("div[itemprop=people]");
+        String sexString = metaData.select("meta[itemprop=gender]").first().attr("content");
+        if ("Male".equals(sexString)) {
+            user.setSex(0);
+        } else if ("Female".equals(sexString)) {
+            user.setSex(1);
+        } else {
+            user.setSex(2);
+        }
 
-                    // 回答数量
+        // 回答数量
 //                    String answersNum = userUrlContent.select(".ProfileMain-header").first().select("li").get(1).text().substring(2);
-                    user.setAnswersNum(userContent.substring(userContent.indexOf("回答")+2,userContent.indexOf("分享")-1));
+        user.setAnswersNum(metaData.select("meta[itemprop=gender]").first().attr("zhihu:answerCount"));
 
-                    // 被赞同数
+        // 被赞同数
 //                    Elements e1 = userUrlContent.select(".IconGraf");
 //                    String starsNumString = e1.get(e1.size() - 2).text();
 //                    String starsNum = starsNumString.substring(starsNumString.indexOf("得") + 2, starsNumString.indexOf("次") - 1);
 
-            user.setStarsNum(userContent.substring(userContent.indexOf("获得")+3,userContent.indexOf("次赞同")-1));
-            user.setThxNum(userContent.substring(userContent.lastIndexOf("获得")+3,userContent.indexOf("次感谢")-1));
+        user.setStarsNum(metaData.select("meta[itemprop=gender]").first().attr("zhihu:voteupCount"));
+        user.setThxNum(metaData.select("meta[itemprop=gender]").first().attr("zhihu:thankedCount"));
 
-                    // 被感谢数
-//                    Elements e2 = userUrlContent.select(".Profile-sideColumnItemValue");
-//                    String thxNumString = e2.get(e2.size() - 1).text();
-//                    String thxNum = thxNumString.substring(thxNumString.indexOf("得") + 2, thxNumString.indexOf("次") - 1);
+        //关注的人
+        user.setFollowingNum(userUrlContent.select(".NumberBoard-itemValue").get(0).text());
 
-
-                    //关注的人
-                    String followingNum = userUrlContent.select(".NumberBoard-value").first().text();
-                    user.setFollowingNum(followingNum);
-
-                    //关注者数量
-                    String followersNum = userUrlContent.select(".NumberBoard-value").get(1).text();
-                    user.setFollowersNum(followersNum);
+        //关注者数量
+        user.setFollowersNum(userUrlContent.select(".NumberBoard-itemValue").get(1).text());
 
 
-                    //打印用户信息
-                    System.out.println("爬取成功：" + user);
+        //打印用户信息
+        System.out.println("爬取成功：" + user);
 
-                    String sql = "insert into user " +
-                            "(name,sex,business,company,position,education,major,answersNum,starsNum,thxNum,followingNum,followersNum) " +
-                            "values (?,?,?,?,?,?,?,?,?,?,?,?)";
-                    try {
-                        ps = con.prepareStatement(sql, Statement.SUCCESS_NO_INFO);
-                        ps.setString(1, user.getName());
-                        ps.setInt(2, user.getSex());
-                        ps.setString(3, user.getBusiness());
-                        ps.setString(4, user.getCompany());
-                        ps.setString(5, user.getPosition());
-                        ps.setString(6, user.getEducation());
-                        ps.setString(7, user.getMajor());
-                        ps.setString(8, user.getAnswersNum());
-                        ps.setString(9, user.getStarsNum());
-                        ps.setString(10, user.getThxNum());
-                        ps.setString(11, user.getFollowingNum());
-                        ps.setString(12, user.getFollowersNum());
-                        //存储user
-                        ps.executeUpdate();
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    //把用户所关注的人的url加入到阻塞队列
-                    addUserFollowingUrl(tmp);
-
+        String sql = "insert into user " +
+                "(name,sex,business,company,position,education,major,answersNum,starsNum,thxNum,followingNum,followersNum) " +
+                "values (?,?,?,?,?,?,?,?,?,?,?,?)";
+        try {
+            ps = con.prepareStatement(sql, Statement.SUCCESS_NO_INFO);
+            ps.setString(1, user.getName());
+            ps.setInt(2, user.getSex());
+            ps.setString(3, user.getBusiness());
+            ps.setString(4, user.getCompany());
+            ps.setString(5, user.getPosition());
+            ps.setString(6, user.getEducation());
+            ps.setString(7, user.getMajor());
+            ps.setString(8, user.getAnswersNum());
+            ps.setString(9, user.getStarsNum());
+            ps.setString(10, user.getThxNum());
+            ps.setString(11, user.getFollowingNum());
+            ps.setString(12, user.getFollowersNum());
+            //存储user
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //把用户所关注的人的url加入到阻塞队列
+        addUserFollowingUrl(tmp);
     }
 
     //传入用户url，获取他所关注人的url
-    public static void addUserFollowingUrl(String userUrl){
+    public static void addUserFollowingUrl(String userUrl) {
         int i = 1;
         String userFollowingUrl = "";
-            userFollowingUrl = userUrl+"/following?page=" + i;
-            i++;
-            Element userFollowingContent = null;
-            userFollowingContent = Jsoup.parse(getFromUrl(userFollowingUrl));
-            Elements followingElements = userFollowingContent.select(".List-item");
-            //判断当前页关注人数是否为0，是的话就跳出循环
-            if (followingElements.size() != 0) {
-                for (Element e : followingElements) {
-                    String newUserUrl = e.select("a[href]").get(0).attr("href");
-                    //把获取到的地址加入阻塞队列
-                    try {
-                        if (!newUserUrl.contains("org")) {
-                            urlQueue.put("https://www.zhihu.com" + newUserUrl);
-                        }
-                    } catch (InterruptedException e1) {
-                        e1.printStackTrace();
+        userFollowingUrl = userUrl + "/following?page=" + i;
+        i++;
+        Element userFollowingContent = null;
+        userFollowingContent = Jsoup.parse(getFromUrl(userFollowingUrl));
+        Elements followingElements = userFollowingContent.select(".List-item");
+        //判断当前页关注人数是否为0，是的话就跳出循环
+        if (followingElements.size() != 0) {
+            for (Element e : followingElements) {
+                String newUserUrl = e.select("a[href]").get(0).attr("href");
+                //把获取到的地址加入阻塞队列
+                try {
+                    if (!newUserUrl.contains("org")) {
+                        urlQueue.put("https:" + newUserUrl);
                     }
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
                 }
             }
+        }
     }
 
     static class ErrHandler implements Thread.UncaughtExceptionHandler {
@@ -272,7 +265,7 @@ public class spriderService {
                             if (tmp != null) {
                                 crawler(tmp);
                             }
-                        }else {
+                        } else {
                             System.out.println("此url存在，不爬了." + tmp);
                         }
                     }
